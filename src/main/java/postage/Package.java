897743b19f70cafbdc778869408906adcb154611
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2019-09-04 20:39:39 +0100

    Decomposed conditional to clarify code

--- src/main/java/postage/Package.java
+++ src/main/java/postage/Package.java
@@ -14,12 +14,32 @@ public class Package {
     }
 
     public double postageInBaseCurrency() {
-        if (weight <= 60 && height <= 229 && width <= 162 && depth <= 25) {
-            return 120;
+        if (isSmall()) {
+            return smallPostageInBaseCurrency();
         }
-        if (weight <= 500 && height <= 324 && width <= 229 && depth <= 100) {
-            return weight * 4;
+        if (isMedium()) {
+            return mediumPostageInBaseCurrency();
         }
+        return largePostageInBaseCurrency();
+    }
+
+    private double largePostageInBaseCurrency() {
         return Math.max(weight, height * width * depth / 1000) * 6;
+    }
+
+    private double mediumPostageInBaseCurrency() {
+        return weight * 4;
+    }
+
+    private double smallPostageInBaseCurrency() {
+        return 120;
+    }
+
+    private boolean isMedium() {
+        return weight <= 500 && height <= 324 && width <= 229 && depth <= 100;
+    }
+
+    private boolean isSmall() {
+        return weight <= 60 && height <= 229 && width <= 162 && depth <= 25;
     }
 }
