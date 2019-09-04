7e29ae739d78b4c997db0671c4d8658d580e6910
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2019-09-04 21:13:03 +0100

    Moved creation logic to abstract factory class

--- src/main/java/postage/Package.java
+++ src/main/java/postage/Package.java
@@ -2,31 +2,14 @@ package postage;
 
 public class Package {
 
-    private final SizedPackage aPackage;
+    private final SizedPackage sizedPackage;
 
     public Package(int weight, int height, int width, int depth) {
-        aPackage = createPackage(depth, height, weight, width);
+        sizedPackage = SizedPackage.createSizedPackage(depth, height, weight, width);
     }
 
     public double postageInBaseCurrency() {
-        return aPackage.postageInBaseCurrency();
+        return sizedPackage.postageInBaseCurrency();
     }
 
-    private static SizedPackage createPackage(int depth, int height, int weight, int width) {
-        if (isSmall(depth, height, weight, width)) {
-            return new SmallPackage();
-        }
-        if (isMedium(depth, height, weight, width)) {
-            return new MediumPackage(weight);
-        }
-        return new LargePackage(depth, height, weight, width);
-    }
-
-    private static boolean isMedium(int depth, int height, int weight, int width) {
-        return weight <= 500 && height <= 324 && width <= 229 && depth <= 100;
-    }
-
-    private static boolean isSmall(int depth, int height, int weight, int width) {
-        return weight <= 60 && height <= 229 && width <= 162 && depth <= 25;
-    }
 }
--- src/main/java/postage/SizedPackage.java
+++ src/main/java/postage/SizedPackage.java
@@ -1,5 +1,23 @@
 package postage;
 
 public abstract class SizedPackage {
+    static SizedPackage createSizedPackage(int depth, int height, int weight, int width) {
+        if (isSmall(depth, height, weight, width)) {
+            return new SmallPackage();
+        }
+        if (isMedium(depth, height, weight, width)) {
+            return new MediumPackage(weight);
+        }
+        return new LargePackage(depth, height, weight, width);
+    }
+
+    private static boolean isMedium(int depth, int height, int weight, int width) {
+        return weight <= 500 && height <= 324 && width <= 229 && depth <= 100;
+    }
+
+    private static boolean isSmall(int depth, int height, int weight, int width) {
+        return weight <= 60 && height <= 229 && width <= 162 && depth <= 25;
+    }
+
     abstract double postageInBaseCurrency();
 }
