b13999d1dd19ea665f98980facafceeddcec6e0d
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2019-09-04 21:09:19 +0100

    Removed temporary field

--- src/main/java/postage/Package.java
+++ src/main/java/postage/Package.java
@@ -1,39 +1,32 @@
 package postage;
 
 public class Package {
-    private final int weight;
-    private final int height;
-    private final int width;
-    private final int depth;
+
     private final SizedPackage aPackage;
 
     public Package(int weight, int height, int width, int depth) {
-        this.weight = weight;
-        this.height = height;
-        this.width = width;
-        this.depth = depth;
-        aPackage = createPackage();
+        aPackage = createPackage(depth, height, weight, width);
     }
 
     public double postageInBaseCurrency() {
         return aPackage.postageInBaseCurrency();
     }
 
-    private SizedPackage createPackage() {
-        if (isSmall()) {
+    private static SizedPackage createPackage(int depth, int height, int weight, int width) {
+        if (isSmall(depth, height, weight, width)) {
             return new SmallPackage();
         }
-        if (isMedium()) {
+        if (isMedium(depth, height, weight, width)) {
             return new MediumPackage(weight);
         }
         return new LargePackage(depth, height, weight, width);
     }
 
-    private boolean isMedium() {
+    private static boolean isMedium(int depth, int height, int weight, int width) {
         return weight <= 500 && height <= 324 && width <= 229 && depth <= 100;
     }
 
-    private boolean isSmall() {
+    private static boolean isSmall(int depth, int height, int weight, int width) {
         return weight <= 60 && height <= 229 && width <= 162 && depth <= 25;
     }
 }
